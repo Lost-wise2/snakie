15bd030adb6dbc92061dd516b29a48b80a9c03f0
Lost-wise2 <nori.rice0@gmail.com>
2026-04-22 23:50:01 +0200

    remade the basics of the game in pygame instead

--- main_game.py
+++ main_game.py
@@ -1,3 +1,33 @@
 import pygame
+import sys
 
 pygame.init()
+
+#game definitions
+clock = pygame.time.Clock()
+
+
+TileSize = 25
+NumberTiles = 30
+
+SCREEN_WIDTH = TileSize*NumberTiles
+SCREEN_HEIGHT = TileSize*TileSize
+
+
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Snakie")
+
+
+
+#The game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    #Frame rate and background
+    clock.tick(60)
+    screen.fill((255,255,255))
+    pygame.display.flip()

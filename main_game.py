88ddd0afe6a1aa9991a698180d5bcb674914b175
Lost-wise2 <nori.rice0@gmail.com>
2026-04-22 19:31:01 +0200

    changed from tkinter to pygame as I have more experience with that anyways

--- main_game.py
+++ main_game.py
@@ -1,35 +1,3 @@
-#This should be the main game file
-import tkinter
-import random
+import pygame
 
-Rows = 30
-Cols = 25
-TileSize = 25
-
-Window_Width = TileSize * Rows
-Window_Height = TileSize * Cols
-
-#game windoww
-Window = tkinter.Tk()
-Window.title("Snakie")
-Window.resizable(False, False)
-
-Canvas = tkinter.Canvas(Window, bg= "black", width=Window_Width, height=Window_Height, borderwidth=0, highlightthickness=0)
-Canvas.pack()
-Window.update()
-
-#location
-window_width = Window.winfo_width()
-window_height = Window.winfo_height()
-screen_width = Window.winfo_screenwidth()
-screen_height = Window.winfo_screenheight()
-
-Window_x = int((screen_width/2) - (window_width/2))
-Window_y = int((screen_height/2) - (window_height/2))
-
-Window.geometry(f"{window_width}x{window_height}+{Window_x}+{Window_y}")
-
-
-
-
-Window.mainloop()
+pygame.init()

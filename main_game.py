ed91634ac85a50d2dd10d0320e6373d26530e3e2
Lost-wise2 <nori.rice0@gmail.com>
2026-04-28 22:42:10 +0200

    added random positioning of fruit as well as beta image, added the base class for snakie too.

--- main_game.py
+++ main_game.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.math import Vector2
 import sys
+import random
 
 pygame.init()
 
@@ -19,15 +20,30 @@ SCREEN_HEIGHT = TileSize*TileSize
 
 class Fruit:
     def __init__(self):
-        self.position = Vector2(5,6)
+        self.position = self.generRandomPos()
 
 
     def draw(self):
         fruitRect = pygame.Rect(self.position.x * TileSize, self.position.y * TileSize, TileSize, TileSize)
-        pygame.draw.rect(screen, BLACK, fruitRect)
+        #pygame.draw.rect(screen, BLACK, fruitRect)
+        screen.blit(fruitSurface, fruitRect)
+
+    def generRandomPos(self):
+        x = random.randint(0, NumberTiles-1)
+        y = random.randint(0, NumberTiles-1)
+        position = Vector2(x,y)
+        return position
 
 
 
+class Snakie:
+    def __init__(self):
+        self.body = [Vector2(6,9), Vector2(5,9), Vector2(4,9)]
+
+    def draw(self):
+        for segment in self.body:
+            segmentRect = (segment.x * TileSize, segment.y *TileSize, TileSize, TileSize)
+            pygame.draw.rect(screen, BLACK, segmentRect, 0,7)
 
 
 
@@ -37,6 +53,8 @@ pygame.display.set_caption("Snakie")
 
 
 fruit = Fruit()
+snakie = Snakie()
+fruitSurface = pygame.image.load("test_fuit.jpg")
 
 
 #The game loop
@@ -46,10 +64,13 @@ while running:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+
     #Frame rate and background
     clock.tick(60)
     screen.fill((255,255,255))
     fruit.draw()
+    snakie.draw()
 
 
 
